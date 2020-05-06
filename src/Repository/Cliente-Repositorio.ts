416cda234e66f7cc6e2ac6b354033a9e06314cb5
kherrera <kenher822@gmail.com>
2020-05-05 22:39:25 -0600

    Add Repository folder and Cliente-Repositorio.ts file

---
+++ src/Repository/Cliente-Repositorio.ts
@@ -0,0 +1,33 @@
+import { getManager, UpdateResult, DeleteResult } from 'typeorm'
+import { Cliente } from '../Entities/Cliente';
+
+export class ClienteRepositorio{
+
+    crearcliente(cliente:Cliente):Promise<Cliente>{
+        return getManager().getRepository(Cliente).save(cliente);
+    }
+
+    obtenerListaClientes():Promise<Cliente[]>{
+        return getManager().getRepository(Cliente).createQueryBuilder('Cliente')
+        .select(['Cliente.id', 'Cliente.nombre', 'Cliente.apallido'])
+        .getMany(); // getOne
+    }
+
+    obtenerCliente(idCliente: number):Promise<Cliente>{
+        return getManager().getRepository(Cliente).findOne({
+            where:{
+                id:idCliente
+            }
+        });
+    }
+
+    actualizarCliente(idCliente: string, nuevosDatosCliente:Cliente):Promise<UpdateResult>{
+        return getManager().getRepository(Cliente).update({id:idCliente}, nuevosDatosCliente);
+    }
+
+    eliminarCliente(idCliente:string):Promise<DeleteResult>{
+        return getManager().getRepository(Cliente).delete({id:idCliente});
+    }
+
+
+}
